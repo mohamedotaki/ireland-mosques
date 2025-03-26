e8af457f0a17f4b614a0ce4025d21e39b103828e
mohamedotaki <52335186+mohamedotaki@users.noreply.github.com>
2025-03-26 14:07:46 +0000

    Update SignInSignUp.tsx

--- src/components/SignInSignUp.tsx
+++ src/components/SignInSignUp.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, TextField, Button, Typography, Box, Grid } from '@mui/material';
+import { Container, TextField, Button, Typography, Box, Grid, StepIconClassKey } from '@mui/material';
 import { UserSignupType, UserType } from '../types/user';
 import { apiPost } from '../utils/api';
 import { useAuth } from '../hooks/AuthContext';
@@ -11,6 +11,8 @@ import { useAuth } from '../hooks/AuthContext';
 const SignInSignUp = () => {
   const [isSignUp, setIsSignUp] = useState(false); // toggle between SignUp and SignIn
   const [loading, setLoading] = useState(false); // toggle between SignUp and SignIn
+  const [verification, setVerification] = useState(true); // toggle between SignUp and SignIn
+  const [verificationCode, setVerificationCode] = useState(""); // toggle between SignUp and SignIn
   const [user, setUser] = useState<UserSignupType>({
     email: '',
     password: ''
@@ -22,38 +24,42 @@ const SignInSignUp = () => {
 
   // Handle form submission (SignUp or SignIn)
   const handleFormSubmit = async (event: React.FormEvent) => {
+    setLoading(true)
     event.preventDefault();
     setError("")
-    if (user.password.length < 8) {
-      setBadPassword(true)
-      return
-    }
-    if (isSignUp) {
-      if (user.password !== user.confirmPassword) {
-        setBadPassword(true)
-        return
-      }
-      badPassword && setBadPassword(false)
-      setLoading(true)
-      const { data, error } = await apiPost('auth/signup', { user })
-      setLoading(false)
-      data && setIsSignUp(false)
-      error && setError(error || "")
-      setUser({
-        email: '',
-        password: ''
-      })
-    } else {
-      setLoading(true)
-      const { data, error } = await apiPost<{ user: UserSignupType }, UserType>('auth/signin', { user })
+    if (verification) {
+      const { data, error } = await apiPost<{ verificationCode: string }, UserType>('auth/verify', { verificationCode })
       if (data) {
         login(data)
       } else {
         setError(error || "")
       }
-      badPassword && setBadPassword(false)
-      setLoading(false)
+    } else {
+      if (user.password.length < 8 || user.password !== user.confirmPassword) {
+        setBadPassword(true)
+        return
+      }
+      if (isSignUp) {
+        badPassword && setBadPassword(false)
+        const { data, error } = await apiPost('auth/signup', { user })
+        console.log(data, error)
+        if (data) {
+          setVerification(true)
+        } else {
+          setError(error || "")
+        }
+      } else {
+        const { data, error } = await apiPost<{ user: UserSignupType }, UserType>('auth/signin', { user })
+        if (data) {
+          setVerification(true)
+        } else {
+          setError(error || "")
+        }
+        badPassword && setBadPassword(false)
+      }
     }
+    setLoading(false)
+
   };
   const handleGoogleLogin = (response: any) => {
     console.log("Google Login Success", response);
@@ -66,6 +72,8 @@ const SignInSignUp = () => {
     // Implement further login logic
   };
 
+
+
   const handleFieldChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setUser((prevUser) => ({
@@ -73,93 +81,118 @@ const SignInSignUp = () => {
       [name]: value,
     }));
   };
-
   return (
     <Container maxWidth="xs" sx={{ mt: 8 }}>
       <Typography variant="h4" align="center" gutterBottom>
-        {isSignUp ? 'Sign Up' : 'Sign In'}
+        {!verification ? isSignUp ? 'Sign Up' : 'Sign In' : "Verification"}
       </Typography>
 
       <form onSubmit={handleFormSubmit}>
-        {isSignUp && <TextField
-          fullWidth
-          name='name'
-          label="Full Name"
-          variant="outlined"
-          margin="normal"
-          type="text"
-          required
-          value={user.name}
-          onChange={handleFieldChange}
-        />}
-        <TextField
-          fullWidth
-          name='email'
-          label="Email"
-          variant="outlined"
-          margin="normal"
-          type="email"
-          required
-          value={user.email}
-          onChange={handleFieldChange}
-        />
-        <TextField
-          fullWidth
-          name='password'
-          label="Password"
-          variant="outlined"
-          margin="normal"
-          type="password"
-          required
-          value={user.password}
-          onChange={handleFieldChange}
-          error={badPassword}
+        {
+          verification ? (
+            <>
+              <TextField
+                fullWidth
+                name='verificationCode'
+                label="Verification Code"
+                variant="outlined"
+                margin="normal"
+                type="text"
+                required
+                value={verificationCode}
+                onChange={(e) => setVerificationCode(e.target.value)}
+              />
+              <Typography variant="body2" sx={{ mt: 2, textAlign: "center" }}>Please Check your inbox or junk</Typography>
+            </>
+          ) : (
+            <>
+              {isSignUp && (
+                <>
+                  <TextField
+                    fullWidth
+                    name='name'
+                    label="Full Name"
+                    variant="outlined"
+                    margin="normal"
+                    type="text"
+                    required
+                    value={user.name}
+                    onChange={handleFieldChange}
+                  />
+                  <TextField
+                    fullWidth
+                    name='phoneNumber'
+                    label="Phone Number"
+                    variant="outlined"
+                    margin="normal"
+                    type="tel"
+                    inputMode="tel"
+                    required
+                    value={user.phoneNumber}
+                    onChange={handleFieldChange}
+                  />
+                </>
+              )}
 
-        />
+              <TextField
+                fullWidth
+                name='email'
+                label="Email"
+                variant="outlined"
+                margin="normal"
+                type="email"
+                required
+                value={user.email}
+                onChange={handleFieldChange}
+              />
+              <TextField
+                fullWidth
+                name='password'
+                label="Password"
+                variant="outlined"
+                margin="normal"
+                type="password"
+                required
+                value={user.password}
+                onChange={handleFieldChange}
+                error={badPassword}
+              />
 
-        {/* Conditional fields for SignUp */}
-        {isSignUp && (
-          <>
-            <TextField
-              fullWidth
-              name='confirmPassword'
-              label="Confirm Password"
-              variant="outlined"
-              margin="normal"
-              type="password"
-              required
-              value={user.confirmPassword}
-              onChange={handleFieldChange}
-              error={badPassword}
-            />
-            <TextField
-              fullWidth
-              name='phoneNumber'
-              label="Phone Number"
-              variant="outlined"
-              margin="normal"
-              type="tel"
-              inputMode="tel"
-              required
-              value={user.phoneNumber}
-              onChange={handleFieldChange}
+              {/* Conditional field for Confirm Password (only visible during SignUp) */}
+              {isSignUp && (
+                <TextField
+                  fullWidth
+                  name='confirmPassword'
+                  label="Confirm Password"
+                  variant="outlined"
+                  margin="normal"
+                  type="password"
+                  required
+                  value={user.confirmPassword}
+                  onChange={handleFieldChange}
+                  error={badPassword}
+                />
+              )}
+            </>
+          )
+        }
 
 
-            />
-          </>
-        )}
+
         {error && <Typography variant="body2" sx={{ mt: 2, color: 'red', textAlign: "center" }}>{error}</Typography>
         }
         <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 2 }}>
           <Button loading={loading} type="submit" variant="contained" color="primary" fullWidth>
-            {isSignUp ? 'Sign Up' : 'Sign In'}
+            {!verification ? isSignUp ? 'Sign Up' : 'Sign In' : "Verify"}
           </Button>
-          <Typography variant="body2" sx={{ mt: 2 }}>
+          {!verification && <Typography variant="body2" sx={{ mt: 2 }}>
             {isSignUp ? 'Already have an account?' : "Don't have an account?"}{' '}
             <Button onClick={() => setIsSignUp(!isSignUp)} color="primary">
               {isSignUp ? 'Sign In' : 'Sign Up'}
             </Button>
-          </Typography>
+          </Typography>}
+
+
 
           {/* Social Login Buttons */}
           {/*           <Box sx={{ mt: 3 }}>
